2e3a794912be3f24195ec1fa46fc010b172fb516
RileyJJ794 <hoffman@Hoffmans-MacBook-Pro.local>
2019-12-19 14:10:31 -0600

    fixed css link

--- scripts.js
+++ scripts.js
@@ -1,20 +1,32 @@
 
 
 
-function searchMarvel(searchTerm) {
+function searchPoke(searchTerm) {
     fetch(`https://pokeapi.co/api/v2/pokemon/${searchTerm}`).then(result => {
         return result.json();
     }).then(result => {
-        // displayResults(result);
-        console.log(result)
+        // console.log(result)
+        init(result);
     })
 }
+
+function init(resultFromServer) {
+    let species = resultFromServer['species']['name'];
+
+    document.getElementById('poke-species').innerHTML = species;
+
+    document.body.style.backgroundImage = resultFromServer['sprites']['front_default'];
+};
+
+
+
+
 
 document.getElementById('search-btn').addEventListener('click', () => {
     let searchTerm = document.getElementById('search-input').value;
     console.log('btn clicked');
     if (searchTerm) {
-        searchMarvel(searchTerm);
+        searchPoke(searchTerm);
         console.log(searchTerm);
     }
 })
