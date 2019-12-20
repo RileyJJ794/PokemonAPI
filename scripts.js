2ede478274761588b416d23a733c1d47a0419138
RileyJJ794 <hoffman@Hoffmans-MacBook-Pro.local>
2019-12-20 13:31:34 -0600

    added poke moves

--- scripts.js
+++ scripts.js
@@ -12,13 +12,21 @@ function searchPoke(searchTerm) {
 
 function init(resultFromServer) {
     let species = resultFromServer['species']['name'];
-    let type = resultFromServer['types']['0']['type']['name']
+    let type = resultFromServer['types']['0']['type']['name'];
+    let moves = '';
 
+    for (i=0; i < resultFromServer['moves'].length; i++) {
+        moves += resultFromServer['moves'][i]['move']['name'];
+        moves += '<br/>'
+    }
+
+    document.getElementById('poke-moves').innerHTML = moves;
     document.getElementById('poke-species').innerHTML = species;
     document.getElementById('poke-type').innerHTML = type;
 
     document.body.style.backgroundImage = 'none'
     document.body.style.backgroundColor = 'rgb(200,0,0)';
+    document.getElementById('pokedex').style.visibility = 'visible';
 };
 
 
