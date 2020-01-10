c45ef7b34007042b26dc4438e35d0b81dd2e9af7
RileyJJ794 <hoffman@Hoffmans-MacBook-Pro.local>
2020-01-10 12:45:10 -0600

    last commit for a while

--- scripts.js
+++ scripts.js
@@ -16,7 +16,7 @@ function init(resultFromServer) {
     let image = resultFromServer['sprites']['front_default'];
     let moves = '';
 
-    if (resultFromServer['moves'].length == 1){
+    if (resultFromServer['moves'].length== 1){
         moves += resultFromServer['moves'][0]['move']['name']
     } else {
         for (i=0; i < 4; i++) {
